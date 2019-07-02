3f1e00d06613511a8bbb1396e2b2e0bb66a09fd0
zyandeep <zyandeep000@gmail.com>
2019-07-02 20:35:21 +0530

    learningggg....

--- practice.kt
+++ practice.kt
@@ -1,13 +1,70 @@
-class Alien {
+//import java.math.BigInteger; 
+
+/* class Alien {
     var skill: String? = null
 
     fun show() {
         println(this.skill)
     } 
+} */
+
+///// PRIMARY CONSTRUCTOR AND SECONDARY CONSTRUCTOR
+/* class Human(name: String, age: Int) {
+    var name: String = ""
+    var age: Int = 0
+
+    //// BODY OF PRIMAEY CONSTRUCTOR
+    init {
+        this.name = name
+        this.age = age
+        println("Human created")
+    }
+
+
+    fun think(): Unit {
+        println("Human is $name, age $age")
+    }
+} */
+
+
+/* class Human(private var name: String, private var age: Int) {
+    //// BODY OF PRIMAEY CONSTRUCTOR
+    init {
+        this.name = name
+        this.age = age
+        println("Human created")
+    }
+
+
+    fun think(): Unit {
+        println("Human is $name, age $age")
+    }
+} */
+
+class Human(private var name: String, private var age: Int) {
+    //// BODY OF PRIMAEY CONSTRUCTOR
+    init {
+        println("Human created")
+    }
+
+    ///// EVERY 2NDAY CONSTRUCTOR MUST CALL PRIMARY CONSTRUCTOR
+    constructor(name: String): this(name, 23) {
+        println("2nd-ery constructor called")
+    }
+
+
+    fun think(): Unit = println("Human is $name, age $age")
 }
 
 
+
+
+
 fun main(args: Array<String>) {
+
+    var h = Human("Bob")                /// calling 2nd-ery constructor
+    h.think()
+
     //var res = calcAmount(50, 0.05)
     /* var res = calcAmount(amt = 23)
     println("amount = $res") */
@@ -26,7 +83,7 @@ fun main(args: Array<String>) {
 
     println(res) */
 
-    var a1 = Alien()
+    /* var a1 = Alien()
     a1.skill = "Java"
     a1.show()
 
@@ -38,17 +95,42 @@ fun main(args: Array<String>) {
     //var a3 = a1 plus a2 
 
     var a3 = a1 + a2                     
-    a3.show()
+    a3.show() */
+
+    /* var num: Int = 5
+    var res: Int = 1
+
+    for (i in 1..num) {
+        res *= i
+    } */
+
+    //println(fact(700))
 }
 
+/* tailrec fun fact(num: Int, res: BigInteger = BigInteger.ONE): BigInteger {
+    if(BigInteger.valueOf(num.toLong()) == BigInteger.ZERO) {
+        return res
+    }
+    else {
+        return fact(num - 1, res * BigInteger.valueOf(num.toLong()))
+    }
+} */
 
-///// EXTENTION FUNCTION
-/* fun Alien.plus(a: Alien): Alien {
+
+/*
+fun fact(num: Int): Int = 
+if(num <= 1) 
+    1
+else
+    num * fact(num - 1)
+        
+//EXTENTION FUNCTION
+ fun Alien.plus(a: Alien): Alien {
     var t = Alien()
     t.skill = this.skill + " " + a.skill
     
     return t
-} */
+} 
 
 
 ///// INFIX FUNCTION 
@@ -60,11 +142,9 @@ operator infix fun Alien.plus(a: Alien): Alien {
 }
 
 
-
-
-
 // FUNCTION EXPRESSION
 //fun calcAmount(amt: Int) = (amt + amt * 0.04)
 
 //// DEFAULT PARAMETER
 //fun calcAmount(amt: Int, interest: Double = 0.04) = (amt + amt * interest)
+*/
